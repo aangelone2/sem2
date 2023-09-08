ea19faec3293038594ee0bbd5aef342a11e12e13
aangelone2 <adriano.angelone.work@gmail.com>
2023-09-08 18:04:38 +0200

    added license information

--- modules/crud.py
+++ modules/crud.py
@@ -6,6 +6,31 @@ CRUDHandler
     Class mediating CRUD operations.
 """
 
+# Copyright (c) 2023 Adriano Angelone
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the
+# Software.
+#
+# This file is part of sem.
+#
+# This file may be used under the terms of the GNU General
+# Public License version 3.0 as published by the Free Software
+# Foundation and appearing in the file LICENSE included in the
+# packaging of this file.  Please review the following
+# information to ensure the GNU General Public License version
+# 3.0 requirements will be met:
+# http://www.gnu.org/copyleft/gpl.html.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+# KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+# PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 
 from datetime import date
 import csv
